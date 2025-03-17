7f7df2e8ab33700161ac03c7fb2c74f18c3d4533
MuseCat <66839978+MuseCat77@users.noreply.github.com>
2025-03-17 17:56:08 +0000

    чуть повеселее дашборд

--- dashboard/plotlydash.py
+++ dashboard/plotlydash.py
@@ -1,5 +1,6 @@
-from dash import Dash, html, dash_table
+from dash import Dash, html, dash_table, dcc
 import pandas as pd
+import plotly.express as px
 
 # Incorporate data
 df = pd.read_csv('http://127.0.0.1:8000/api/cpus/?format=csv')
@@ -10,7 +11,8 @@ app = Dash()
 # App layout
 app.layout = [
     html.Div(children='Список процессоров'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+    dash_table.DataTable(data=df.to_dict('records'), page_size=30),
+    dcc.Graph(figure=px.histogram(df, x='vendor', y='die_size', histfunc='avg'))
 ]
 
 # Run the app
